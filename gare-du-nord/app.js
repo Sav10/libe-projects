d219afcd0774378f1ba38d01c445f53b789ef44d
Sav10 <sav.derivet@gmail.com>
2022-02-16 17:20:04 +0100

    data updated

--- gare-du-nord/app.js
+++ gare-du-nord/app.js
@@ -1,5 +1,39 @@
 let allzones;
 
+let all_person = {
+'MathieuSabrinaVelib':{
+'title':"Mathieu et Sabrina 1",
+ "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+'url': "https://www.liberation.fr"},
+'Disparus':{
+'title':"Les disparus",
+ "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+'url': "https://www.liberation.fr"},
+'AnitaDoctor':{
+'title':"Anita",
+ "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+'url': "https://www.liberation.fr"},
+'HorlogeGareDuNord':{
+'title':"L'horloge",
+ "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+'url': "https://www.liberation.fr"},
+'OdileCarnet':{
+'title':"Les carnets d'Odile",
+ "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+'url': "https://www.liberation.fr"},
+'JeanPierre':{
+'title':"Jean-Pierre",
+ "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+'url': "https://www.liberation.fr"},
+'MathieuSabrinaTrain':{
+'title':"Mathieu et Sabrina",
+ "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+'url': "https://www.liberation.fr"},
+'OdileEquipe':{'title':"L'équipe d'Odile",
+ "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+'url': "https://www.liberation.fr"}
+}
+
 
 d3.xml("plan-gdn_1.svg")
   .then(data => {
@@ -22,8 +56,10 @@ allZones
 .style('fill', 'red')
 .style('opacity', 0)
 .on('mouseover', function(){
-let that = this;
-console.log(d3.select(that).attr('id'))
+
+let_this_id = d3.select(this).attr('id')
+
+console.log(all_person[let_this_id].title)
 }
 	)
 
