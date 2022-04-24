352338f9b804798b6a5f7e7647017aac128f6c72
Sav10 <sav.derivet@gmail.com>
2022-04-24 20:43:18 +0200

    data updated

--- election-presidentielle-2022-T2/js/app.js
+++ election-presidentielle-2022-T2/js/app.js
@@ -1631,4 +1631,22 @@ region :
   }
 
 
-const dep_code_names = {"01": "Ain", "02": "Aisne", "03": "Allier", "04": "Alpes-de-Haute-Provence", "05": "Hautes-Alpes", "06": "Alpes-Maritimes", "07": "Ard\\u00e8che", "08": "Ardennes", "09": "Ari\\u00e8ge", "10": "Aube", "11": "Aude", "12": "Aveyron", "13": "Bouches-du-Rh\\u00f4ne", "14": "Calvados", "15": "Cantal", "16": "Charente", "17": "Charente-Maritime", "18": "Cher", "19": "Corr\\u00e8ze", "21": "C\\u00f4te-d\'Or", "22": "C\\u00f4tes-d\'Armor", "23": "Creuse", "24": "Dordogne", "25": "Doubs", "26": "Dr\\u00f4me", "27": "Eure", "28": "Eure-et-Loir", "29": "Finist\\u00e8re", "2A": "Corse-du-Sud", "2B": "Haute-Corse", "30": "Gard", "31": "Haute-Garonne", "32": "Gers", "33": "Gironde", "34": "H\\u00e9rault", "35": "Ille-et-Vilaine", "36": "Indre", "37": "Indre-et-Loire", "38": "Is\\u00e8re", "39": "Jura", "40": "Landes", "41": "Loir-et-Cher", "42": "Loire", "43": "Haute-Loire", "44": "Loire-Atlantique", "45": "Loiret", "46": "Lot", "47": "Lot-et-Garonne", "48": "Loz\\u00e8re", "49": "Maine-et-Loire", "50": "Manche", "51": "Marne", "52": "Haute-Marne", "53": "Mayenne", "54": "Meurthe-et-Moselle", "55": "Meuse", "56": "Morbihan", "57": "Moselle", "58": "Ni\\u00e8vre", "59": "Nord", "60": "Oise", "61": "Orne", "62": "Pas-de-Calais", "63": "Puy-de-D\\u00f4me", "64": "Pyr\\u00e9n\\u00e9es-Atlantiques", "65": "Hautes-Pyr\\u00e9n\\u00e9es", "66": "Pyr\\u00e9n\\u00e9es-Orientales", "67": "Bas-Rhin", "68": "Haut-Rhin", "69": "Rh\\u00f4ne", "70": "Haute-Sa\\u00f4ne", "71": "Sa\\u00f4ne-et-Loire", "72": "Sarthe", "73": "Savoie", "74": "Haute-Savoie", "76": "Seine-Maritime", "77": "Seine-et-Marne", "78": "Yvelines", "79": "Deux-S\\u00e8vres", "80": "Somme", "81": "Tarn", "82": "Tarn-et-Garonne", "83": "Var", "84": "Vaucluse", "85": "Vend\\u00e9e", "86": "Vienne", "87": "Haute-Vienne", "88": "Vosges", "89": "Yonne", "90": "Territoire de Belfort", "91": "Essonne", "92": "Hauts-de-Seine", "93": "Seine-Saint-Denis", "94": "Val-de-Marne", "95": "Val-d\'Oise", "ZA": "Guadeloupe", "ZB": "Martinique", "ZC": "Guyane", "ZD": "La R\\u00e9union", "ZS": "Saint-Pierre-et-Miquelon", "ZM": "Mayotte", "ZX": "Saint-Martin/Saint-Barth\\u00e9lemy", "ZW": "Wallis et Futuna", "ZP": "Polyn\\u00e9sie fran\\u00e7aise", "ZN": "Nouvelle-Cal\\u00e9donie", "ZZ": "Fran\\u00e7ais \\u00e9tablis hors de France"}
+const dep_code_names = {"01": "Ain", "02": "Aisne", "03": "Allier", "04": "Alpes-de-Haute-Provence", "05": "Hautes-Alpes",
+ "06": "Alpes-Maritimes", "07": "Ardèche", "08": "Ardennes", "09": "Ariège", "10": "Aube", "11": "Aude", "12": "Aveyron",
+ "13": "Bouches-du-Rhône", "14": "Calvados", "15": "Cantal", "16": "Charente", "17": "Charente-Maritime", "18": "Cher", 
+ "19": "Corrèze", "21": "Côte-d\'Or", "22": "Côtes-d\'Armor", "23": "Creuse", "24": "Dordogne", "25": "Doubs", 
+ "26": "Drôme", "27": "Eure", "28": "Eure-et-Loir", "29": "Finistère", "2A": "Corse-du-Sud", "2B": "Haute-Corse", "30": "Gard", 
+ "31": "Haute-Garonne", "32": "Gers", "33": "Gironde", "34": "Hérault", "35": "Ille-et-Vilaine", "36": "Indre", 
+ "37": "Indre-et-Loire", "38": "Isère", "39": "Jura", "40": "Landes", "41": "Loir-et-Cher", "42": "Loire", "43": "Haute-Loire", 
+ "44": "Loire-Atlantique", "45": "Loiret", "46": "Lot", "47": "Lot-et-Garonne", "48": "Lozère", "49": "Maine-et-Loire", "50": "Manche", 
+ "51": "Marne", "52": "Haute-Marne", "53": "Mayenne", "54": "Meurthe-et-Moselle", "55": "Meuse", "56": "Morbihan", "57": "Moselle", 
+ "58": "Nièvre", "59": "Nord", "60": "Oise", "61": "Orne", "62": "Pas-de-Calais", "63": "Puy-de-Dôme", 
+ "64": "Pyrénées-Atlantiques", "65": "Hautes-Pyrénées", "66": "Pyrénées-Orientales", 
+ "67": "Bas-Rhin", "68": "Haut-Rhin", "69": "Rhône", "70": "Haute-Saône", "71": "Saône-et-Loire", 
+ "72": "Sarthe", "73": "Savoie", "74": "Haute-Savoie", "76": "Seine-Maritime", "77": "Seine-et-Marne", "78": "Yvelines", 
+ "79": "Deux-Sèvres", "80": "Somme", "81": "Tarn", "82": "Tarn-et-Garonne", "83": "Var", "84": "Vaucluse", "85": "Vendée", 
+ "86": "Vienne", "87": "Haute-Vienne", "88": "Vosges", "89": "Yonne", "90": "Territoire de Belfort", "91": "Essonne", 
+ "92": "Hauts-de-Seine", "93": "Seine-Saint-Denis", "94": "Val-de-Marne", "95": "Val-d\'Oise", "ZA": "Guadeloupe", 
+ "ZB": "Martinique", "ZC": "Guyane", "ZD": "La Réunion", "ZS": "Saint-Pierre-et-Miquelon", "ZM": "Mayotte", 
+ "ZX": "Saint-Martin/Saint-Barthélemy", "ZW": "Wallis et Futuna", "ZP": "Polynésie française", 
+ "ZN": "Nouvelle-Calédonie", "ZZ": "Français établis hors de France", "75": "Paris"}
