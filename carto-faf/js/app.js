08df11b52678cb614a6a2e39166df0cbed03af9e
Savinien de Rivet <Sav10@users.noreply.github.com>
2023-07-26 13:20:30 +0200

    Update app.js

--- carto-faf/js/app.js
+++ carto-faf/js/app.js
@@ -7,6 +7,8 @@ let those_filters = [0,0,0]
 
 let all_filtered_classes
 
+let objClassColor = {}
+
 obj_group_class =  {
     "Les Natifs": "TIF",
     "Némésis": "NEM",
@@ -63,6 +65,45 @@ obj_group_class =  {
 
 
 
+let objVillesClasses = 
+{
+    "Albi": "Albi",
+    "Angers": "Angers",
+    "Beauvais": "Beauvais",
+    "Besancon": "Besançon",
+    "Bordeaux": "Bordeaux",
+    "Bourg-en-Bresse": "Bourg-en-Bresse",
+    "Bourges": "Bourges",
+    "Chambery": "Chambéry/Grenoble/Annecy",
+    "Clermont": "Clermont-Ferrand",
+    "Dijon": "Dijon",
+    "Etroussat": "Etroussat",
+    "Laval": "Laval",
+    "Lille": "Lille",
+    "Lyon": "Lyon",
+    "Aix-Marseille": "Marseille/Aix",
+    "Metz": "Metz",
+    "Moulin": "Moulin",
+    "Nancy": "Nancy",
+    "Nantes": "Nantes",
+    "Nice": "Nice",
+    "Orleans": "Orléans",
+    "Paris": "Paris/Versailles",
+    "Perpignan": "Perpignan",
+    "Reims": "Reims",
+    "Rennes": "Rennes",
+    "Rouen": "Rouen",
+    "St-Julien-de-la-Nef": "Saint-Julien-de-la-Nef",
+    "Sees": "Sées",
+    "Toulouse": "Toulouse",
+    "Tours": "Tours",
+    "Vannes": "Vannes",
+    "Versailles": "Versailles",
+    "Puy-en-Velay": "le Puy-en-Velay"
+}
+
+
+
 
 d3.xml("img/LIB-2023-08-29-SVG-Carte.svg")
 .then(data => {
@@ -99,6 +140,8 @@ d3.selectAll("#CONTOUR circle").nodes().forEach((n) => {
   d3.select('#Fils .' + this_class)
   .style('stroke', this_color)
   .style('stroke-opacity', 1)
+
+  objClassColor[this_class] = this_color
 
 
 });
@@ -138,6 +181,41 @@ d3.selectAll('#Groupes text')
     // d3.select(this)
     // .style('fill-opacity', 1)
   })
+
+
+
+d3.selectAll('#Villes_00000085937572202784161360000007901630108656599683_ circle')
+.on('click', function(event, d) {
+
+  let this_id = d3.select(this).attr('id');
+  console.log(this_id)
+
+  console.log(objVillesClasses[this_id])
+
+  let this_city = objVillesClasses[this_id]
+
+
+d3.selectAll('#Villes_00000085937572202784161360000007901630108656599683_ circle')
+.style('fill-opacity', .3)
+.style('stroke-opacity', .3)
+
+d3.selectAll('#Villes_00000085937572202784161360000007901630108656599683_ circle#' + this_id)
+.style('fill-opacity', 1)
+.style('stroke-opacity', 1)
+
+
+those_filters[0] = this_city
+
+filter_groups('cities')
+
+
+d3.select('#cities-select').property('value', this_city)
+
+
+
+
+  })
+
 
 
 Promise.all([
@@ -260,8 +338,6 @@ else if(selected_type == 'cities'){
 else {
 
 
-  console.log("changing filters 1")
-
 let cities_selection = d3.select('#cities-select').selectAll('option')
 .data(all_villes)
 .join('option')
@@ -273,11 +349,30 @@ d3.select('#cities-select')
 .on('change', function(d){
     let this_prop = d3.select(this).property('value');
     console.log(this_prop)
-    if (this_prop == 'Toutes'){
+    if (this_prop == 'Toutes' || this_prop == ''){
       those_filters[0] = 0;
+
+d3.selectAll('#Villes_00000085937572202784161360000007901630108656599683_ circle')
+.style('fill-opacity', 1)
+.style('stroke-opacity', 1)
+
+
+
         }
     else{
       those_filters[0] = this_prop;
+
+    let this_id = Object.entries(objVillesClasses).filter(d=>d[1] == this_prop)[0][0]
+
+    d3.selectAll('#Villes_00000085937572202784161360000007901630108656599683_ circle')
+.style('fill-opacity', .3)
+.style('stroke-opacity', .3)
+
+d3.selectAll('#Villes_00000085937572202784161360000007901630108656599683_ circle#' + this_id)
+.style('fill-opacity', 1)
+.style('stroke-opacity', 1)
+
+
     }
 
     filter_groups('cities')
@@ -297,7 +392,6 @@ else if(selected_type == 'ideologies'){
 }
 else{
 
-  console.log("changing filters 2")
 
 let ideologies_selection = d3.select('#ideology-select').selectAll('option')
 .data(all_ideologies)
@@ -382,7 +476,9 @@ let this_html
 
 this_html = `<div class="box_element_line">
 <div class="box_element_col"><img width="100" height="100" src="img/logos/${that_class}.jpg"></div>
-<div class="box_element_col" style="margin-left:1em"><span>${this_d.ville}</span><h3>${this_d.nom_groupe}</h3>  <span>${this_d.famille_ideologique}</span></div>
+<div class="box_element_col" style="margin-left:1em"><span>${this_d.ville}</span>
+<h3 style="color:${objClassColor[that_class]}">${this_d.nom_groupe}</h3>  
+<span class="legende_dot"><span class="text_legend">${this_d.famille_ideologique}</span><span class="dot" style="background-color: ${objClassColor[that_class]};"></span></span></div>
 </div>
 <div class="box_element_line">
 Lorem ipsum dolor sit amet. Eos iure nihil et molestiae saepe est voluptatem natus. 
@@ -413,9 +509,14 @@ d3.select('#tooltip')
 d3.select('body').on("click",function(event, d){
 
 
-    var outside = d3.selectAll("#CONTOUR circle, #Groupes text").filter(equalToEventTarget).empty();
+    var outside = d3.selectAll("svg g circle, #Groupes text, .select_menu select").filter(equalToEventTarget).empty();
     // var outside = d3.select(event.path[0]).attr('class') != 'cls-1'
     if (outside) {
+
+
+      console.log('outside')
+
+      console.log(event)
 
       
     // reset_tooltip()
@@ -432,6 +533,29 @@ d3.select('#map_info')
 
 d3.select('#tooltip')
 .style('display', 'none')
+
+
+d3.selectAll('#Villes_00000085937572202784161360000007901630108656599683_ circle')
+.style('fill-opacity', 1)
+.style('stroke-opacity', 1)
+
+
+console.log('changing prop value')
+
+
+d3.select('#cities-select').property('value', '')
+d3.select('#select_ideology').property('value', '')
+d3.select('#group-select').property('value', '')
+
+those_filters = [0, 0, 0]
+
+filtered_data = all_data;
+
+all_filtered_classes = _.uniq(filtered_data.map(d=>d.this_class))
+
+filter_options('')
+
+
 
 
     }
